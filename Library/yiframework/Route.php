1de203d66433f252f474404fcff843c7d856ff5c
yifanes <yifanes@qq.com>
2013-01-14 20:17:18 +0800

    achieve the mvc route and make two global var

---
+++ Library/yiframework/Route.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * des:mvc路由转发
+ */
+class Route{
+	//静态方法
+	public static function run(){
+		//定义默认控制器
+		$controller = empty($_GET['c']) ? 'Index' : trim($_GET['c']);
+		//定义默认方法
+		$action = empty($_GET['a']) ? 'index' : trim($_GET['a']);
+		$controllerBasePath = APP_PATH.'UserApps/Modules/Controllers/';
+		$controllerFilePath = $controllerBasePath .$controller ."Controller.php";
+		if(is_file($controllerFilePath)){
+			include_once $controllerFilePath;
+			$controllerName = $controller . 'Controller';
+			if(class_exists($controllerName)){
+				$controllerHandler = new $controllerName();
+				if(method_exists($controllerName, $action)){
+					$controllerHandler->$action();
+				}else{
+					echo "the method does not exists";
+				}
+			}else{
+				echo "this class not exists";
+			}
+		}else{
+			echo "Controller file not exists";
+		}
+	}
+}
