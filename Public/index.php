255f09919decfc1b1224bfe8da75f8ab937622cb
yifanes <yifanes@qq.com>
2013-01-14 20:15:51 +0800

    achieve the mvc route and make two global var

--- Public/index.php
+++ Public/index.php
@@ -1,1 +1,9 @@
 <?php
+//定义项目根目录
+!defined("APP_PATH") || define('APP_PATH',dirname(__FILE__).'/../');
+//定义框架所在目录
+!defined("FRAMEWORK_PATH") || define('FRAMEWORK_PATH',APP_PATH.'Library/yiframework/');
+
+//调用入口处理文件
+include_once FRAMEWORK_PATH.'Route.php';
+Route::run();
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*
- * 主入口文件
- */
-include dirname(__FILE__)."../Library/Test/Route.php";
-//调用静态方法
-Route::run();
