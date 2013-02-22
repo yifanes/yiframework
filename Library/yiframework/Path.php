31f5da33123469e17f1b166aab67e002dca88dbc
yifanes <yifanes@qq.com>
2013-02-22 17:33:23 +0800

    add route and view handler

---
+++ Library/yiframework/Path.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: zhangxy
+ * Date: 13-2-22
+ * Time: 上午11:15
+ */
+class Path extends Base{
+    private static $_basePath = '';
+    private static $_controller = '';
+    private static $_action = '';
+
+    public static function setAction($action)
+    {
+        self::$_action = $action;
+    }
+
+    public static function getAction()
+    {
+        return self::$_action;
+    }
+
+    public static function setBasePath($base)
+    {
+        self::$_base = $base;
+    }
+
+    public static function getBasePath()
+    {
+        return self::$_base;
+    }
+
+    public static function setController($controller)
+    {
+        self::$_controller = $controller;
+    }
+
+    public static function getController()
+    {
+        return self::$_controller;
+    }
+
+
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 13-2-22
+ * Time: 上午10:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+var_dump($_SERVER);
